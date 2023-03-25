bc7d4a0121320cecf360372a649d106535294424
ltwes606 <ltwes606@gmail.com>
2023-03-25 23:44:46 +0900

    상품 목록 구현

--- item-service/src/main/java/hello/itemservice/domain/item/ItemRepository.java
+++ item-service/src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class ItemRepository {
 
     private static final Map<Long, Item> store = new HashMap<>();
--- item-service/src/main/java/hello/itemservice/domain/item/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/domain/item/basic/BasicItemController.java
@@ -2,16 +2,28 @@ package hello.itemservice.domain.item.basic;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
+import java.util.List;
 import javax.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @RequestMapping("/basic/items")
 @RequiredArgsConstructor
 public class BasicItemController {
 
     private final ItemRepository itemRepository;
-    
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
     /**
      * 테스트용 데이터 추가
      */
