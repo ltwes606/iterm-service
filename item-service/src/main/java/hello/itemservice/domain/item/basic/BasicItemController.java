dfa8c732063c9da29c68af0e00864f2fa9c08cc1
ltwes606 <ltwes606@gmail.com>
2023-03-26 03:32:15 +0900

    상품 수정 구현

--- item-service/src/main/java/hello/itemservice/domain/item/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/domain/item/basic/BasicItemController.java
@@ -84,6 +84,23 @@ public class BasicItemController {
         return "basic/item";
     }
 
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    /**
+     * 스프링에서 redirect와 PathVariable의 값을 사용할 수 있게 해준다.
+     */
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
+    }
+
     /**
      * 테스트용 데이터 추가
      */
