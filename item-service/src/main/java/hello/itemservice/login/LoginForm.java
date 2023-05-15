20874eaa0b1b2d8009698b1274954eaf24e54088
ltwes606 <ltwes606@gmail.com>
2023-05-15 16:57:18 +0900

    로그인 화면 개발

---
+++ item-service/src/main/java/hello/itemservice/login/LoginForm.java
@@ -0,0 +1,13 @@
+package hello.itemservice.login;
+
+import javax.validation.constraints.NotEmpty;
+import lombok.Data;
+
+@Data
+public class LoginForm {
+
+    @NotEmpty
+    private String loginId;
+    @NotEmpty
+    private String password;
+}
---
+++ item-service/src/main/java/hello/itemservice/login/LoginService.java
@@ -0,0 +1,20 @@
+package hello.itemservice.login;
+
+
+import hello.itemservice.login.domain.member.Member.Member;
+import hello.itemservice.login.domain.member.Member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+
+    private final MemberRepository memberRepository;
+
+    public Member login(String loginId, String password) {
+        return memberRepository.findByLoginId(loginId)
+                .filter(m -> m.getPassword().equals(password))
+                .orElse(null);
+    }
+}
